e2be7aa41dd9743e271c9dce839ff8cb6f57ba17
h671430 <111428784+671430@users.noreply.github.com>
2024-02-19 15:03:10 +0100

    Update DeleteTopicMsg.java

--- src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -1,17 +1,26 @@
 package no.hvl.dat110.messages;
 
 public class DeleteTopicMsg extends Message {
+	
+	// TODO: 
+	// Implement objectvariables, constructor, get/set-methods, and toString method
+    private String topic;
 
-	// message sent from client to create topic on the broker
-
-    public DeleteTopicMsg(String user, String topic) {
-
+    public DeleteTopicMsg(String user, String topic){
+        super(MessageType.DELETETOPIC, user);
+        this.topic = topic;
     }
 
-	// TODO:
-	// Implement object variables - a topic is required
+    public String getTopic() {
+        return topic;
+    }
 
-	// Complete the constructor, get/set-methods, and toString method
-	// as described in the project text
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
 
+    @Override
+    public String toString() {
+        return "Topic: " +topic+ " by user: " + super.getUser();
+    }
 }
